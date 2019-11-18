173ae6545e9530a3d478433e3e516a616b39a780
yuanxindong <yuanxin.me@gmail.com>
2019-11-18 15:02:58 +0800

    add: findMoreOrEqualThan function

--- src/util.ts
+++ src/util.ts
@@ -6,3 +6,12 @@ const RADIAN = Math.PI / 180;
 export function angleToRadian(angle: number): number {
   return RADIAN * angle;
 }
+
+export function findMoreOrEqualThan(nums: number[], target: number): number[] {
+  return nums.reduce((prev, num) => {
+    if (num >= target) {
+      prev.push(num);
+    }
+    return prev;
+  }, []);
+}
